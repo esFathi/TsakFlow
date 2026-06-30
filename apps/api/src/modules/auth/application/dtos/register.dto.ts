c0596c6ed46a094f7b52ec6a262a2be9c49850a9
webhouse\amirs <a.siami@webhouse.ir>
2026-06-30 17:28:56 +0330

    ADD register DTO in auth/application

--- apps/api/src/modules/auth/application/dtos/register.dto.ts
+++ apps/api/src/modules/auth/application/dtos/register.dto.ts
@@ -1,1 +1,32 @@
 // Input DTO for registration (email, password, name) with validation rules.
+
+import {
+ IsEmail,
+ IsString,
+ MinLength,
+ MaxLength,
+ Matches
+} from 'class-validator';
+
+export class RegisterDto {
+
+ @IsEmail()
+ email!:string;
+
+ @IsString()
+ @MinLength(3)
+ @MaxLength(100)
+ fullName!:string;
+
+ @IsString()
+ @MinLength(8)
+ @Matches(
+   /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/,
+   {
+     message:
+     'Password too weak'
+   }
+ )
+ password!:string;
+
+}
