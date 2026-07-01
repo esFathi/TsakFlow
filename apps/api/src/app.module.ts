c568bebf7ed1b55ccc74437406913f4c0a85023d
ehsanf <ehsaanfathiofficial@gmail.com>
2026-06-30 23:30:25 -0700

    MODIFY app module to connect to db in best practise

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,55 +1,40 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
-import appConfig from './config/app.config';
-import databaseConfig from './config/database.config';
-import jwtConfig from './config/jwt.config';
+import appConfig from "./config/app.config";
+import databaseConfig from "./config/database.config";
+import jwtConfig from "./config/jwt.config";
 
-import { AuthModule } from './modules/auth/auth.module';
-import { UsersModule } from './modules/users/users.module';
+import { AuthModule } from "./modules/auth/auth.module";
+import { UsersModule } from "./modules/users/users.module";
 
 @Module({
- imports:[
-   ConfigModule.forRoot({
-      isGlobal:true,
-      envFilePath:[
-        '.env',
-        '../../.env'
-      ],
-      load:[
-        appConfig,
-        databaseConfig,
-        jwtConfig
-      ]
-   }),
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [".env", "../../.env"],
+      load: [appConfig, databaseConfig, jwtConfig],
+    }),
 
-   TypeOrmModule.forRoot({
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: "postgres" as const,
+          host: config.get<string>("DB_HOST"),
+          port: Number(config.get<string>("DB_PORT")),
+          username: config.get<string>("DB_USERNAME"),
+          password: config.get<string>("DB_PASSWORD"),
+          database: config.get<string>("DB_NAME"),
+          autoLoadEntities: true,
+          synchronize: true,
+        };
+      },
+    }),
 
-      type:'postgres',
-
-      host:process.env.DB_HOST,
-
-      port:Number(
-        process.env.DB_PORT
-      ),
-
-      username:
-      process.env.DB_USERNAME,
-
-      password:
-      process.env.DB_PASSWORD,
-
-      database:
-      process.env.DB_NAME,
-
-      autoLoadEntities:true,
-
-      synchronize:true
-   }),
-
-   UsersModule,
-   AuthModule
- ]
+    UsersModule,
+    AuthModule,
+  ],
 })
-export class AppModule{}
+export class AppModule {}
